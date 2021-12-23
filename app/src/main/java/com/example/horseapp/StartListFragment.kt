11e1873cb2b4720c578ec9459c3bc996649f2622
hessah <hessah.mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-23 09:15:08 +0300

    create profile

--- app/src/main/java/com/example/horseapp/StartListFragment.kt
+++ app/src/main/java/com/example/horseapp/StartListFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.horseapp.databinding.FragmentStartListBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class StartListFragment : Fragment() {
 
     private var binding: FragmentStartListBinding? = null
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +29,17 @@ class StartListFragment : Fragment() {
         return binding?.root
     }
 
+
+
     override fun onDestroy() {
         super.onDestroy()
 
         // garbage collector
         binding = null
     }
+
+
+
+
+
 }
